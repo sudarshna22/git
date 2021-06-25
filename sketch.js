b37a57546cfb846c796bf584c881ee126b66e1fa
sudarshna22 <sudarshna.agarwal@gmail.com>
2021-06-25 20:02:23 +0530

    variable

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup(){
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
 
@@ -56,7 +56,7 @@ function draw(){
 
     box5.display();
     log4.display();
-    log5.display();
+    log6.display();
 
     bird.display();
 }
